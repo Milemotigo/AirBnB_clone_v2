50a58f2839fb10dbe50554a7ebf46c918cf1320c
keme-ebi <kelvin6bolou@gmail.com>
2023-06-28 22:34:20 +0000

    redid files

--- models/state.py
+++ models/state.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    citites = relationship("City", backref='states')
+
+    @property
+    def cities(self):
+        """returns the list of city instances"""
+        c_inst = []
+        for city in models.storage.all('City').values():
+            if city.state_id == self.id:
+                c_inst.append(city)
+        return c_inst
