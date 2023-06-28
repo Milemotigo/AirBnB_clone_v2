c3e874f63552acaf1972e66b05c956f3f22d3ae7
keme-ebi <kelvin6bolou@gmail.com>
2023-06-28 10:45:03 +0000

    added new files

--- models/state.py
+++ models/state.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade="all, delete", backref='states')
+
+    @property
+    def cities(self):
+        """returns the list of city instances"""
+        c_inst = []
+        for city in models.storage.all('City').values():
+            if city.state_id == self.id:
+                c_inst.append(city)
+        return c_inst
