50734ef31a1e08899cddfcc845b4a2d65eeaa301
milemotigo <mtgo@gmail.com>
2023-07-06 16:17:06 -0700

    task

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
-# Bash script that sets up your web servers for the deployment of web_static
+# ###
 
 sudo apt -y update
+
 sudo apt install -y nginx
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-echo "<h1> fake HTML file </h1>" | sudo tee /data/web_static/releases/test/index.html
+echo "<h1> Static web page test</h1>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
-locate="location /hbnb_static {\n\t\talias /data/web_static/current;\n\t}"
-sudo sed -i "53i\\\t$locate" /etc/nginx/sites-available/default
+
+loc="location /hbnb_static {\n\t\talias /data/web_static/current;\n\t}"
+sudo sed -i "/53i/\\\t$loc" /etc/nginx/sites-available/default
 
 sudo service nginx restart
