1df78c9185a974470aa9097be4009819afca8f2b
milemotigo <mtgo@gmail.com>
2023-07-06 15:09:42 -0700

    task

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Bash script that sets up your web servers for the deployment of web_static.
+
+sudo apt -y update
+sudo apt install -y nginx
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+echo "<h1> fake HTML file </h1>" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
+locate="location /hbnb_static {\n\t\talias /data/web_static/current;\n\t}"
+sudo sed -i "53i\\\t$locate" /etc/nginx/sites-available/default
