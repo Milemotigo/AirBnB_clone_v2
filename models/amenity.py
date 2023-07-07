308609be2f61ffa3632d4d8890aaaf150f355dbf
keme-ebi <kelvin6bolou@gmail.com>
2023-07-07 00:46:52 +0000

    fix

--- models/amenity.py
+++ models/amenity.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import relationship
 from models.place import place_amenity
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
     place_amenities = relationship('Place', secondary=place_amenity)
