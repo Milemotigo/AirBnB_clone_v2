fd62b31edc7f3f402c31cf6021e625e1032609c8
keme-ebi <kelvin6bolou@gmail.com>
2023-07-07 01:03:32 +0000

    fix

--- models/place.py
+++ models/place.py
@@ -5,6 +5,7 @@ from sqlalchemy import Column, Table, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import relationship
 from models.review import Review
 import models
+import os
 
 place_amenity = Table("place_amenity", Base.metadata,
                       Column('place_id', String(60), ForeignKey('places.id'),
@@ -28,26 +29,30 @@ class Place(BaseModel, Base):
     latitude = Column(Float)
     longitude = Column(Float)
     amenity_ids = []
-    reviews = relationship("Review", cascade='all, delete', backref='place')
-    amenities = relationship("Amenity", secondary=place_amenity,
-                             viewonly=False, back_populates="place_amenities")
 
-    @property
-    def reviews(self):
-        """returns the list of review instances"""
-        r_inst = []
-        for review in models.storage.all(Review).values():
-            if review.place_id == self.id:
-                r_inst.append(review)
-        return r_inst
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        reviews = relationship("Review", cascade='all, delete',
+                               backref='place')
+        amenities = relationship("Amenity", secondary=place_amenity,
+                                 viewonly=False)
 
-    @property
-    def amenities(self):
-        """returns the list of amenity instances"""
-        return self.amenity_ids
+    else:
+        @property
+        def reviews(self):
+            """returns the list of review instances"""
+            r_inst = []
+            for review in models.storage.all(Review).values():
+                if review.place_id == self.id:
+                    r_inst.append(review)
+            return r_inst
 
-    @amenities.setter
-    def amenities(self, a_obj=None):
-        """handles append method"""
-        if isinstance(a_obj, Amenity):
-            self.amenity_ids.append(a_obj.id)
+        @property
+        def amenities(self):
+            """returns the list of amenity instances"""
+            return self.amenity_ids
+
+        @amenities.setter
+        def amenities(self, a_obj=None):
+            """handles append method"""
+            if isinstance(a_obj, Amenity):
+                self.amenity_ids.append(a_obj.id)
