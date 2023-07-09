f7a10ba5c53096183575652bf0efd4133be729d8
milemotigo <mtigo@gmail.com>
2023-07-09 19:00:13 +0100

    task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,13 +8,13 @@ from datetime import datetime
 
 def do_pack():
     """
-    Returns the archive path if archive has been
+    returns the archive path if archive has been
     correctly gernerated else return nothimg
     """
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     local('mkdir -p versions')
     tar_path = local('tar -c -z -v -f versions/web_static_{}.tgz web_static'
-                    .format(timestamp))
+                     .format(timestamp))
     if tar_path.success:
         return tar_path
     else:
