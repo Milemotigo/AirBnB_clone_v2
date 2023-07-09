fa465f574e0c2ace5986917184290ea0a39ddcbc
milemotigo <mtigo@gmail.com>
2023-07-09 18:46:23 +0100

    task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,18 +1,18 @@
-#!/usr/bin/python3 
- """a script to pack static content into a tarball fro m web_static folder
- """ 
- from fabric.api import local
- from datetime import datetime
-  
-  
- def do_pack():
-     """packages all contents from web_static into .tgz archive
-     """
-     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-     local('mkdir -p versions')
-     Obj_tar = local('tar -c -z -v -f versions/web_static_{}.tgz web_static'
+#!/usr/bin/python3
+"""a script to pack static content into a tarball fro m web_static folder
+"""
+from fabric.api import local
+from datetime import datetime.now()
+
+
+def do_pack():
+    """packages all contents from web_static into .tgz archive
+    """
+    timestamp = datetime.strftime("%Y%m%d%H%M%S")
+    local('mkdir -p versions')
+    Obj_tar = local('tar -c -z -v -f versions/web_static_{}.tgz web_static'
                     .format(timestamp))
-     if Obj_tar.failed:
-         return None
-     else:
-         return Obj_tar
+    if Obj_tar.failed:
+        return None
+    else:
+        return Obj_tar
