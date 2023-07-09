2ff3631dbac096642669ed29c6b2761111dba34a
milemotigo <mtigo@gmail.com>
2023-07-09 18:47:10 +0100

    task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,8 +6,6 @@ from datetime import datetime.now()
 
 
 def do_pack():
-    """packages all contents from web_static into .tgz archive
-    """
     timestamp = datetime.strftime("%Y%m%d%H%M%S")
     local('mkdir -p versions')
     Obj_tar = local('tar -c -z -v -f versions/web_static_{}.tgz web_static'
