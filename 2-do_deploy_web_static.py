40299a7dbcaf1fb8b46c59867d1bb05b0a71f21e
milemotigo <mtigo@gmail.com>
2023-07-10 05:16:18 +0100

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,53 +1,33 @@
-#!/usr/bin/env python3
-'''a script to pack static content into a tarball
-'''
-
-from fabric.api import *
-from datetime import datetime
-
-
-env.hosts = ['3.83.18.183', '54.157.147.102']
-env.users = "ubuntu"
-
-
-def do_pack():
-    """
-    returns the archive path if archive has been
-    correctly gernerated else return nothimg
-    """
-    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    local('mkdir -p versions')
-    archive_path = local('tar -c -z -v -f versions/web_static_{}.tgz\
-            web_static'.format(timestamp))
-    if archive_path.success:
-        return archive_path
-    else:
-        return None
-
-
-def do_deploy(archive_path):
-    """
+#!/usr/bin/python3
+ """ 
     Fabric script that distributes an archive to your web servers,
     using the function
-    """
-    if os.path.exists(archive_path):
-        archive = archive_path[9:]
-        fileversion = "/data/web_static/releases/" + archive_file[:-4]
-        archive = "/tmp/" + archive_file
-
-        put(archive, "/tmp/")
-        run("sudo mkdir -p {}".format(fileversion))
-
-        run("sudo tar -xzf {} -C {}/".format(archive_file, fileversion))
-        run("sudo rm -rf {}".format(archive))
-        run("sudo mv {}/web_static/* {}".format(fileversion,
-                                                fileversion))
-
-        run("sudo rm -rf {}/web_static".format(fileversion))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {} /data/web_static/current".format(fileversion))
-
-        print("fileversion deployed!")
-        return True
-
-    return False
+ """
+ from fabric.api import *
+ import os.path
+ env.hosts = ['34.148.10.186', '34.74.50.110']
+ env.user = 'ubuntu'
+  
+  
+ def do_deploy(archive_path):
+     """
+     Deploy archive to web server
+     """
+     if os.path.isfile(archive_path) is None:
+         return False
+     try:
+         file = archive_path.split("/")[-1]
+         rm_ext = filename.split(".")[0]
+         path_rm_ext = "/data/web_static/releases/{}/".format(rm_ext)
+         symlink = "/data/web_static/current"
+         put(archive_path, "/tmp/")
+         run("mkdir -p {}".format(path_rm_ext)) 
+         run("tar -xzf /tmp/{} -C {}".format(file, path_rm_ext))
+         run("rm /tmp/{}".format(file))
+         run("mv {}web_static/* {}".format(path_rm_ext, path_rm_ext)) 
+         run("rm -rf {}web_static".format(path_rm_ext))
+         run("rm -rf {}".format(symlink)) 
+         run("ln -s {} {}".format(path_rm_ext, symlink))
+         return True
+     except:
+         return False
