4816b7d4f8e1675c972e326fc30dfeebb66d5638
milemotigo <mtgo@gmail.com>
2023-07-14 14:47:58 -0700

    fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
-'''a script to pack static content into a tarball
-'''
-
+"""a script to pack static content into a tarball
+"""
 from fabric.api import *
 from datetime import datetime
 
 
 def do_pack():
+    """packages all contents from web_static into .tgz archive
     """
-    returns the archive path if archive has been
-    correctly generated else return nothing
-    """
-    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
     local('mkdir -p versions')
-    archive_path = 'versions/web_static_{}.tgz web_static'.format(timestamp)
-    command = "tar -czvf {} web_static".format(timestamp)
-    put = local(command)
+    result = local('tar -czvf versions/web_static_{}.tgz web_static'.format(now))
     if result.failed:
         return None
     else:
-        return put
+        return result
