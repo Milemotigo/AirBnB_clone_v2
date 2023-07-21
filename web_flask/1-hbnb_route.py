6bb1f6d8c1e65d59360fbdf1277b8bdc5c5bbad1
milemotigo <mtigo@gmail.com>
2023-07-21 21:11:38 +0100

    task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,7 +11,7 @@ def hello_hbnb():
     return "Hello HBNB!"
 
 
-@app.route("/HBNB", strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
     '''function with /HBNB route '''
     return "HBNB"
