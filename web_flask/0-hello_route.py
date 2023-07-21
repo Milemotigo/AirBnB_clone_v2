4cf64e9cb93eda4de3267b587a6b1fe6f86aaaa7
milemotigo <mtigo@gmail.com>
2023-07-21 20:17:42 +0100

    task 1

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# a script that starts a Flask web application
+
+from flask import Flask
+
+app = Flask(__name__, strict_slashes=False)
+@app.route("/")
+def hello_hbnb():
+    return "Hello HBNB!"
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', port = '5000')
