752ed5d16476bf57e933fdc0e25646e1f3a5cbb5
milemotigo <mtigo@gmail.com>
2023-07-21 21:01:21 +0100

    task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# a script that starts a Flask web application
+''' a script that starts a Flask web application'''
 
 from flask import Flask
 app = Flask(__name__)
@@ -7,8 +7,10 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
+    ''' function with / route '''
     return "Hello HBNB!"
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    ''' main function '''
+    app.run(host='0.0.0.0', port=5000)
