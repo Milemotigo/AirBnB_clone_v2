55a1003fb8471176bc3fd74aa9e58274505891a6
milemotigo <mtigo@gmail.com>
2023-07-22 18:43:08 +0100

    task5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -38,12 +38,10 @@ def display_is_number(n):
     return "{:d} is a number".format(n)
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
+'''@app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
     '''display a HTML page only if n is an integer'''
-    return render_template("5-number.html", n=n)
-
-
+    return render_template("5-number.html", n=n)'''
 if __name__ == '__main__':
     '''main function '''
     app.run(host='0.0.0.0', port=5000)
