70635f0f519a36801538909f56df5cd6dc7478ee
milemotigo <mtgo@gmail.com>
2023-07-23 15:49:27 -0700

    task7

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,7 +11,10 @@ class FileStorage:
     def all(self, cls=None):
         """Returns a dictionary or list of models currently in storage"""
         if cls is not None:
-            return {key: obj for key, obj in FileStorage.__objects.items() if isinstance(obj, cls)}
+            return {key: obj for key,
+                    obj in FileStorage.__objects.items()
+                    if isinstance(obj, cls)
+                    }
         return FileStorage.__objects
 
     def new(self, obj):
@@ -47,9 +50,10 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
     def delete(self, obj=None):
         '''Deletes an object from the file dictionary'''
         if obj is None:
@@ -57,6 +61,7 @@ class FileStorage:
         else:
             key = obj.__class__.__name__ + '.' + obj.id
             FileStorage.__objects.pop(key, None)
+
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
