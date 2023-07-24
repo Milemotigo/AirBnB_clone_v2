15ea62d4cddf70ff7b64ddba3cfa370377e3a7af
milemotigo <mtgo@gmail.com>
2023-07-23 19:01:44 -0700

    task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
-""" a script that starts a Flask web application"""
+"""
+a script that starts a Flask web application
+"""
+
 from flask import Flask, render_template
-from models import storage, State
-from sqlalchemy.orm import scoped_session, sessionmaker
+from models import storage
+from models.state import State
+
 
 app = Flask(__name__)
 
 
-Session = scoped_session(sessionmaker(bind=storage._engine))
-
-
 @app.teardown_appcontext
-def close_session(exception=None):
-    """Close the current SQLAlchemy Session after each request."""
-    Session.close()
+def close_appcontext(exception):
+    """close the current SQLAlchemy Session After each request"""
+    storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """Display a HTML page with the list of all State objects sorted by."""
-    states = storage.all(State)
-    sorted_states = sorted(states.values(), key=lambda state: state.name)
-    return render_template('states_list.html', states=sorted_states)
+@app.route("/states_list", strict_slashes=False)
+def stateList():
+    """list all state object (sorted)"""
+    states = storage.all(State).values()
+    stateSorted = sorted(states, key=lambda state: state.name)
+    return render_template('7-states_list.html', states=stateSorted)
 
 
 if __name__ == '__main__':
+    """main function"""
     app.run(host='0.0.0.0', port=5000)
